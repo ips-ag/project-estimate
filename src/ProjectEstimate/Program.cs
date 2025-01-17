59ab84cb575426351a9bafc71407c34c43fbef93
Uroš Miletić <uros.miletic@ips-ag.com>
2025-01-17 15:57:03 +0100

    Create light control agent project (#2)

---
+++ src/ProjectEstimate/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ProjectEstimate;
+using ProjectEstimate.Configuration;
+using Serilog;
+
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .CreateLogger();
+
+// create .NET host using defaults
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(builder => builder.AddJsonFile("secrets.json", optional: false, reloadOnChange: true))
+    .ConfigureLogging(
+        builder =>
+        {
+            builder.ClearProviders();
+            builder.AddSerilog();
+        })
+    .ConfigureServices(
+        services =>
+        {
+            services.AddHostedService<ChatService>();
+            services.AddOptions<AzureOpenAiSettings>().BindConfiguration("Azure:OpenAi");
+        });
+
+await host.RunConsoleAsync();
