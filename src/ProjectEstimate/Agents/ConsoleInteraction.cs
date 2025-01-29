9a0cb582afe2a97810ad5e00a602534d0cd628d1
Uroš Miletić <uros.miletic@ips-ag.com>
2025-01-29 14:54:15 +0100

    User can enter initial requirements in more than one line

--- src/ProjectEstimate/Agents/ConsoleInteraction.cs
+++ src/ProjectEstimate/Agents/ConsoleInteraction.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace ProjectEstimate.Agents;
 
 internal class ConsoleInteraction : IUserInteraction
@@ -6,6 +8,19 @@ internal class ConsoleInteraction : IUserInteraction
     {
         await Console.Out.WriteAsync("User > ");
         return await Console.In.ReadLineAsync(cancel);
+    }
+
+    public async ValueTask<string?> ReadMultilineUserMessageAsync(CancellationToken cancel)
+    {
+        await Console.Out.WriteAsync("User (double Enter to submit)> ");
+        var stringBuilder = new StringBuilder();
+        string? line;
+        do
+        {
+            line = await Console.In.ReadLineAsync(cancel);
+            if (!string.IsNullOrEmpty(line)) stringBuilder.AppendLine(line);
+        } while (!string.IsNullOrEmpty(line));
+        return stringBuilder.Length == 0 ? null : stringBuilder.ToString();
     }
 
     public ValueTask WriteAssistantMessageAsync(string message, CancellationToken cancel)
