9bc14d55465a1c1c23ce5459465d225aa4ab7248
Uroš Miletić <uros.miletic@ips-ag.com>
2025-04-28 10:10:53 +0200

    Cleanup

--- src/api/Application/Request/Context/RequestContextAccessor.cs
+++ src/api/Application/Request/Context/RequestContextAccessor.cs
@@ -14,13 +14,10 @@ internal class RequestContextAccessor : IRequestContextAccessor
             var holder = CurrentContext.Value;
             if (holder != null)
             {
-                // Clear current HttpContext trapped in the AsyncLocals, as its done.
                 holder.Context = null;
             }
             if (value != null)
             {
-                // Use an object indirection to hold the HttpContext in the AsyncLocal,
-                // so it can be cleared in all ExecutionContexts when its cleared.
                 CurrentContext.Value = new RequestContextHolder { Context = value };
             }
         }
--- src/api/Repositories/Hubs/ChatHub.cs
+++ src/api/Repositories/Hubs/ChatHub.cs
@@ -4,9 +4,4 @@ namespace ProjectEstimate.Repositories.Hubs;
 
 internal class ChatHub : Hub<IChatClient>
 {
-    // [HubMethodName("ReceiveMessage")]
-    // public async Task ReceiveMessage(string assistant, string message)
-    // {
-    //     await Clients.All.ReceiveMessage(assistant, message);
-    // }
 }
