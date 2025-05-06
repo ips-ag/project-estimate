76d1ad360f307a8bd9319a0a65cd16dceaacb518
Uroš Miletić <uros.miletic@ips-ag.com>
2025-05-06 15:39:18 +0200

    Refactor repository extensions

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,15 +1,7 @@
 using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.Extensions.Options;
-using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.ChatCompletion;
-using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
 using ProjectEstimate.Application.Extensions;
 using ProjectEstimate.Extensions.ApplicationInsights;
-using ProjectEstimate.Repositories.Agents.Analyst;
-using ProjectEstimate.Repositories.Agents.Architect;
-using ProjectEstimate.Repositories.Agents.Consultant;
-using ProjectEstimate.Repositories.Agents.Developer;
-using ProjectEstimate.Repositories.Configuration;
+using ProjectEstimate.Repositories.Extensions;
 using ProjectEstimate.Repositories.Hubs;
 using Serilog;
 
@@ -28,29 +20,7 @@ try
     builder.Services.AddSignalR();
     builder.Services.AddSingleton<ITelemetryInitializer, ExceptionSamplingRateTelemetryInitializer>();
     builder.Services.AddApplication();
-    builder.Services.AddOptions<AzureOpenAiSettings>()
-        .BindConfiguration(AzureOpenAiSettings.SectionName)
-        .ValidateDataAnnotations()
-        .ValidateOnStart();
-    builder.Services.AddSingleton<IChatCompletionService>(sp =>
-    {
-        var options = sp.GetRequiredService<IOptions<AzureOpenAiSettings>>().Value;
-        var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
-        return new AzureOpenAIChatCompletionService(
-            options.DeploymentName,
-            options.Endpoint,
-            options.ApiKey,
-            loggerFactory: loggerFactory);
-    });
-    builder.Services.AddKeyedTransient<Kernel>("ConsultantAgent", (sp, _) => new Kernel(sp));
-    builder.Services.AddScoped<ConsultantAgent>();
-    builder.Services.AddKeyedTransient<Kernel>("AnalystAgent", (sp, _) => new Kernel(sp));
-    builder.Services.AddScoped<AnalystAgent>();
-    builder.Services.AddKeyedTransient<Kernel>("ArchitectAgent", (sp, _) => new Kernel(sp));
-    builder.Services.AddScoped<ArchitectAgent>();
-    builder.Services.AddKeyedTransient<Kernel>("DeveloperAgent", (sp, _) => new Kernel(sp));
-    builder.Services.AddScoped<DeveloperAgent>();
-    builder.Services.AddSingleton<IUserInteraction, SignalrUserInteraction>();
+    builder.Services.AddRepositories();
 
     var app = builder.Build();
     if (app.Environment.IsDevelopment())
