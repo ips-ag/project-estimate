00a7a219a922146377bc0484cc9bfb215b1eb8ba
Uroš Miletić <uros.miletic@ips-ag.com>
2025-07-25 09:35:00 +0200

    Cleanup

--- src/api/Controllers/ConversationController.cs
+++ src/api/Controllers/ConversationController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Threading.Channels;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using ProjectEstimate.Application.Models;
 
 namespace ProjectEstimate.Controllers;
@@ -9,26 +8,23 @@ namespace ProjectEstimate.Controllers;
 [ApiController]
 public class ConversationController : ControllerBase
 {
-    private readonly ProblemDetailsFactory _problemDetailsFactory;
     private readonly Channel<ChatCompletionRequestModel> _workQueue;
 
-    public ConversationController(
-        ProblemDetailsFactory problemDetailsFactory,
-        Channel<ChatCompletionRequestModel> workQueue)
+    public ConversationController(Channel<ChatCompletionRequestModel> workQueue)
     {
-        _problemDetailsFactory = problemDetailsFactory;
         _workQueue = workQueue;
     }
 
     [HttpPost("", Name = nameof(CompleteConversation))]
-    [ProducesResponseType(typeof(ChatCompletionResponseModel), 200)]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(503)]
+    [ProducesErrorResponseType(typeof(ProblemDetails))]
     public IActionResult CompleteConversation([FromBody] ChatCompletionRequestModel requestModel)
     {
         if (!_workQueue.Writer.TryWrite(requestModel))
         {
             return Problem(statusCode: StatusCodes.Status503ServiceUnavailable);
         }
-        var responseModel = new ChatCompletionResponseModel { Output = string.Empty };
-        return Ok(responseModel);
+        return Ok();
     }
 }
